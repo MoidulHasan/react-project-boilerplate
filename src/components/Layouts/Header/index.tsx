00c0ec5f76535be4ee7c2f6abda1ef078880d4d7
Antukhan <antu.khan.988@gmail.com>
2023-03-04 16:13:28 +0600

    refactor: headers responsiveness incresed

--- src/components/Layouts/Header/index.tsx
+++ src/components/Layouts/Header/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { PATH } from '../../../constants/paths';
 import './style.scss'
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export const AppHeader = () => {
 
@@ -13,37 +14,47 @@ export const AppHeader = () => {
     </>
 
     return (
-        <div className='nav-container'>
+        <div className='nav-container flex justify-center'>
             <div className='content-area flex justify-between items-center'>
                 <div>
                     <img className='nav-logo' src="./logo.svg" alt="site logo" />
                 </div>
 
-                <div className='flex justify-between items-center'>
-                    <div className="">
-                        <input type="text" className="search-field" placeholder="Search" />
+                <div className='hidden md:flex justify-end items-center '>
+                    <div className='flex justify-between items-center'>
+                        <div className="">
+                            <input type="text" className="search-field" placeholder="Search" />
+                        </div>
+
+                        <ul className='flex justify-between items-center'>
+                            {menuItems}
+                        </ul>
                     </div>
 
-                    <ul className='flex justify-between items-center'>
-                        {menuItems}
-                    </ul>
+                    <div className='flex justify-between items-center'>
+
+                        <button className='nav-action-button login-button-color '>
+                            Login
+                        </button>
+
+                        <button className='nav-action-button text-white signup-button-color'>
+                            Signup
+                        </button>
+
+                        <button className='p-0 download-from-playstore'>
+                            <img src="./download-from-playstore.svg" alt="download-from-playstore" className='' />
+                        </button>
+
+                    </div>
                 </div>
 
-                <div className='flex justify-between items-center'>
-
-                    <button className='nav-action-button login-button-color '>
-                        Login
+                <div className='md:hidden'>
+                    <button className='cursor-pointer p-2'>
+                        <FontAwesomeIcon icon={faBars} />
                     </button>
-
-                    <button className='nav-action-button text-white signup-button-color'>
-                        Signup
-                    </button>
-
-                    <button className='p-0'>
-                        <img src="./download-from-playstore.svg" alt="download-from-playstore" />
-                    </button>
-
                 </div>
+
+
             </div>
 
         </div>
